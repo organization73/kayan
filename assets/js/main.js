b62ad5726d870368dc0ab9a5299b7fe58b9cedd5
Elsharabasy73 <abdo.make73@gmail.com>
2024-07-23 22:40:29 +0300

    [Add] test deployment

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+fetch("http://localhost:8080/api/ping")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
