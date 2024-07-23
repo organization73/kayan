24a51840fb78b0bf26caeb6b16f455caa15ab47f
Elsharabasy73 <abdo.make73@gmail.com>
2024-07-23 23:41:00 +0300

    [Add] testing deployment button

---
+++ assets/js/test-btn.js
@@ -0,0 +1,14 @@
+function sendRequest() {
+  console.log("Hello World");
+
+  fetch("http://localhost:8080/api/ping")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
