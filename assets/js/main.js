859e08198e9bbf604afc8f664db443c95446492d
shalabycr7 <abdoshalaby.dev@gmail.com>
2024-07-25 13:50:28 +0300

    fix nav bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,40 @@
-console.log("Hello World");
+// Burger menus
+document.addEventListener("DOMContentLoaded", function () {
+	// open
+	const burger = document.querySelectorAll(".navbar-burger");
+	const menu = document.querySelectorAll(".navbar-menu");
 
-fetch("http://localhost:8080/api/ping")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+	if (burger.length && menu.length) {
+		for (var i = 0; i < burger.length; i++) {
+			burger[i].addEventListener("click", function () {
+				for (var j = 0; j < menu.length; j++) {
+					menu[j].classList.toggle("hidden");
+				}
+			});
+		}
+	}
+
+	// close
+	const close = document.querySelectorAll(".navbar-close");
+	const backdrop = document.querySelectorAll(".navbar-backdrop");
+
+	if (close.length) {
+		for (var i = 0; i < close.length; i++) {
+			close[i].addEventListener("click", function () {
+				for (var j = 0; j < menu.length; j++) {
+					menu[j].classList.toggle("hidden");
+				}
+			});
+		}
+	}
+
+	if (backdrop.length) {
+		for (var i = 0; i < backdrop.length; i++) {
+			backdrop[i].addEventListener("click", function () {
+				for (var j = 0; j < menu.length; j++) {
+					menu[j].classList.toggle("hidden");
+				}
+			});
+		}
+	}
+});
