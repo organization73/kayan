fc16d8fb75420f9e320fd6a2344b15b438b24ade
shalabycr7 <abdoshalaby.dev@gmail.com>
2024-07-28 09:06:36 +0300

    main hero section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Example from "../components/NavBar";
 import Hero from "../components/MainHero";
 import FooterSection from "../components/FooterSection";
 
@@ -11,6 +10,7 @@ const HomePage = () => {
 				<h1 className="font-poppins">about Page</h1>
 				<p>Browse wfwffffffffff products here.</p>
 			</div>
+
 			<FooterSection></FooterSection>
 		</>
 	);
