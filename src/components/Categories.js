de996da5aaa7e4f27b4f5ebdfd2ccf89fc611ba8
shalabycr7 <abdoshalaby.dev@gmail.com>
2024-07-29 13:00:16 +0300

    created categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -6,74 +6,84 @@ import cat4 from "../assets/img/c4.png";
 import cat5 from "../assets/img/c5.png";
 import cat6 from "../assets/img/c6.png";
 import cat7 from "../assets/img/c7.png";
-import cat8 from "../assets/img/c7.png";
+import cat8 from "../assets/img/c8.png";
 
 function Categories() {
 	return (
 		<div className="container mx-auto py-6">
 			<div className="text-center">
-				<div className="text-3xl mb-4">تسوق حسب الفئات</div>
+				<div className="text-3xl mb-6">تسوق حسب الفئات</div>
 				<div className="flex flex-wrap justify-center gap-3">
 					{/* Category Card 1 */}
-					<div className="w-1/2 sm:w-1/3 lg:w-auto">
-						<div className="category-card flex flex-col items-center">
-							<div className="img-container bg-custom p-3 rounded-full bg-gray-100">
-								<img src={cat1} alt="مكتب" className="w-full rounded-full" />
+					<div className="sm:w-1/3 lg:w-auto">
+						<div className="flex flex-col items-center">
+							<div className="p-2 rounded-full bg-gray-100">
+								<img src={cat1} alt="مكتب" className="w-28 rounded-full" />
 							</div>
-							<div className="category-text mt-2">مكتب</div>
+							<div className="mt-2">مكتب</div>
 						</div>
 					</div>
 					{/* Category Card 2 */}
-					<div className="w-1/2 sm:w-1/3 lg:w-auto">
-						<div className="category-card flex flex-col items-center">
-							<div className="img-container bg-custom p-3 rounded-full bg-gray-100">
-								<img src={cat2} alt="غرف نوم" className="w-full rounded-full" />
+					<div className="sm:w-1/3 lg:w-auto">
+						<div className="flex flex-col items-center">
+							<div className="p-2 rounded-full bg-gray-100">
+								<img src={cat2} alt="غرف نوم" className="w-28 rounded-full" />
 							</div>
-							<div className="category-text mt-2">غرف نوم</div>
+							<div className="mt-2">غرف نوم</div>
 						</div>
 					</div>
 					{/* Category Card 3 */}
-					<div className="w-1/2 sm:w-1/3 lg:w-auto">
-						<div className="category-card flex flex-col items-center">
-							<div className="img-container bg-custom p-3 rounded-full bg-gray-100">
-								<img
-									src={cat3}
-									alt="غرف سفرة"
-									className="w-full rounded-full"
-								/>
+					<div className="sm:w-1/3 lg:w-auto">
+						<div className="flex flex-col items-center">
+							<div className="p-2 rounded-full bg-gray-100">
+								<img src={cat3} alt="غرف سفرة" className="w-28 rounded-full" />
 							</div>
-							<div className="category-text mt-2">غرف سفرة</div>
+							<div className="mt-2">غرف سفرة</div>
 						</div>
 					</div>
 					{/* Category Card 4 */}
-					<div className="w-1/2 sm:w-1/3 lg:w-auto">
-						<div className="category-card flex flex-col items-center">
-							<div className="img-container bg-custom p-3 rounded-full bg-gray-100">
-								<img
-									src={cat4}
-									alt="غرف اطفال"
-									className="w-full rounded-full"
-								/>
+					<div className="sm:w-1/3 lg:w-auto">
+						<div className="flex flex-col items-center">
+							<div className="p-2 rounded-full bg-gray-100">
+								<img src={cat4} alt="غرف اطفال" className="w-28 rounded-full" />
 							</div>
-							<div className="category-text mt-2">غرف اطفال</div>
+							<div className="mt-2">غرف اطفال</div>
 						</div>
 					</div>
 					{/* Category Card 5 */}
-					<div className="w-1/2 sm:w-1/3 lg:w-auto">
-						<div className="category-card flex flex-col items-center">
-							<div className="img-container bg-custom p-3 rounded-full bg-gray-100">
-								<img src={cat5} alt="صالونات" className="w-full rounded-full" />
+					<div className="sm:w-1/3 lg:w-auto">
+						<div className="flex flex-col items-center">
+							<div className="p-2 rounded-full bg-gray-100">
+								<img src={cat5} alt="صالونات" className="w-28 rounded-full" />
 							</div>
-							<div className="category-text mt-2">صالونات</div>
+							<div className="mt-2">صالونات</div>
 						</div>
 					</div>
 					{/* Category Card 6 */}
-					<div className="w-1/2 sm:w-1/3 lg:w-auto">
-						<div className="category-card flex flex-col items-center">
-							<div className="img-container bg-custom p-3 rounded-full bg-gray-100">
-								<img src={cat6} alt="نيش" className="w-full rounded-full" />
+					<div className="sm:w-1/3 lg:w-auto">
+						<div className="flex flex-col items-center">
+							<div className="p-2 rounded-full bg-gray-100">
+								<img src={cat6} alt="نيش" className="w-28 rounded-full" />
 							</div>
-							<div className="category-text mt-2">نيش</div>
+							<div className="mt-2">نيش</div>
+						</div>
+					</div>
+					{/* Category Card 7 */}
+					<div className="sm:w-1/3 lg:w-auto">
+						<div className="flex flex-col items-center">
+							<div className="p-2 rounded-full bg-gray-100">
+								<img src={cat7} alt="طاولة" className="w-28 rounded-full" />
+							</div>
+							<div className="mt-2">طاولة</div>
+						</div>
+					</div>
+					{/* Category Card 8 */}
+					<div className="sm:w-1/3 lg:w-auto">
+						<div className="flex flex-col items-center">
+							<div className="p-2 rounded-full bg-gray-100">
+								<img src={cat8} alt="ركنات" className="w-28 rounded-full" />
+							</div>
+							<div className="mt-2">ركنات</div>
 						</div>
 					</div>
 				</div>
