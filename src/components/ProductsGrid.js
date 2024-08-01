9a9c2f6a54eafdc3403857ded22699e62ade9915
Elsharabasy73 <abdo.make73@gmail.com>
2024-08-01 22:01:21 +0300

    [Add] setup products request and it's parametes

--- src/components/ProductsGrid.js
+++ src/components/ProductsGrid.js
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { url } from "../dummyData/baseUrl";
+
 
 async function fetchProducts() {
   try {
-    const response = await fetch("http://localhost:8080/api/client/products");
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
+		const response = await axios.get(url+"/api/client/products", {
+			params: {
+				page: 1,
+				//["bedroom", "office", "kids-room", "dining-room", "sofa", "salon", "table", "cabinet"];
+				category: "bedroom",
+				search: "غرفه",
+				sortBY: "recent",
+			}
+		});
+		const data = response.data
     return data;
   } catch (error) {
     console.error("Error fetching products:", error);
@@ -79,29 +87,31 @@ console.log(products);
 // ];
 
 export default function ProductsGrid() {
-	return (
-		<div className="bg-white">
-			<div className="mx-auto max-w-2xl px-4 py-3 sm:px-6 sm:py-2 lg:max-w-7xl lg:px-8">
-				<h2 className="sr-only">Products</h2>
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-3 sm:px-6 sm:py-2 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">Products</h2>
 
-				<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-					{products.map((product) => (
-						<Link key={product.id} to={product.href} className="group">
-							<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-								<img
-									alt={product.title}
-									src={product.mainImageUrl}
-									className="h-full w-full object-cover object-center group-hover:opacity-75"
-								/>
-							</div>
-							<h3 className="mt-4 text-normal font-medium text-gray-700">{product.title}</h3>
-							{/* <p className="mt-1 text-lg font-medium text-gray-900">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <Link key={product.id} to={product.href} className="group">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                <img
+                  alt={product.title}
+                  src={product.mainImageUrl}
+                  className="h-full w-full object-cover object-center group-hover:opacity-75"
+                />
+              </div>
+              <h3 className="mt-4 text-normal font-medium text-gray-700">
+                {product.title}
+              </h3>
+              {/* <p className="mt-1 text-lg font-medium text-gray-900">
 								{product.price}
 							</p> */}
-						</Link>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
