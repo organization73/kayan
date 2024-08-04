abe9aaef76c22f0793a852d3a3a6fe73eb94344c
shalabycr7 <abdoshalaby.dev@gmail.com>
2024-08-04 17:07:59 +0300

    initial design for contact form

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,6 +4,7 @@ import AboutBanner from "../components/AboutBanner";
 import AboutContactCards from "../components/AboutContactCards";
 import AboutFeaturesCards from "../components/AboutFeaturesCards";
 import FooterSection from "../components/FooterSection";
+import ComplainContact from "../components/ComplainContact";
 
 const AboutPage = () => {
 	return (
@@ -13,7 +14,9 @@ const AboutPage = () => {
 				title="معلومات عنا"
 				subtitle="متجر كيان هو متجر للهدايا والديكورات مقره في مصر. تأسس منذ عام 2019. خدمتنا للعملاء دائمًا مستعدة لدعمكم على مدار 24 ساعة طوال أيام الأسبوع."
 			/>
+
 			<AboutContactCards></AboutContactCards>
+			<ComplainContact></ComplainContact>
 			<AboutFeaturesCards></AboutFeaturesCards>
 			<FooterSection></FooterSection>
 		</>
