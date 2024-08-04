59f2a455b30291f2df25e96fcb060b8b73225c85
shalabycr7 <abdoshalaby.dev@gmail.com>
2024-08-04 17:54:08 +0300

    only show shop sub header if there is an offer

--- src/components/ShopHeader.js
+++ src/components/ShopHeader.js
@@ -22,51 +22,53 @@ function ShopHeader({ offer }) {
 				</div>
 			</section>
 
-			<section className="my-8 dark:bg-dark">
-				<div className="mx-4 lg:mx-8 sm:container">
-					<div className="border-r-[5px] border-primary pr-5">
-						<h2 className="mb-4 text-xl lg:text-2xl tracking-tight">
-							يشمل هذا العرض
-						</h2>
-						<ul className="flex flex-col lg:flex-row">
-							{offer?.categories?.map((category) => (
-								<li
-									className="flex text-base text-body-color ml-4"
-									key={category}
-								>
-									<span className="mt-0.5 text-primary">
-										<svg
-											className="ml-2"
-											width={20}
-											height={20}
-											viewBox="0 0 20 20"
-											fill="none"
-											xmlns="http://www.w3.org/2000/svg"
-										>
-											<g clipPath="url(#clip0_980_24852)">
-												<path
-													d="M10 0.5625C4.78125 0.5625 0.5625 4.78125 0.5625 10C0.5625 15.2188 4.78125 19.4688 10 19.4688C15.2188 19.4688 19.4688 15.2188 19.4688 10C19.4688 4.78125 15.2188 0.5625 10 0.5625ZM10 18.0625C5.5625 18.0625 1.96875 14.4375 1.96875 10C1.96875 5.5625 5.5625 1.96875 10 1.96875C14.4375 1.96875 18.0625 5.59375 18.0625 10.0312C18.0625 14.4375 14.4375 18.0625 10 18.0625Z"
-													fill="currentColor"
-												/>
-												<path
-													d="M12.6875 7.09375L8.96875 10.7188L7.28125 9.0625C7 8.78125 6.5625 8.8125 6.28125 9.0625C6 9.34375 6.03125 9.78125 6.28125 10.0625L8.28125 12C8.46875 12.1875 8.71875 12.2813 8.96875 12.2813C9.21875 12.2813 9.46875 12.1875 9.65625 12L13.6875 8.125C13.9688 7.84375 13.9688 7.40625 13.6875 7.125C13.4063 6.84375 12.9688 6.84375 12.6875 7.09375Z"
-													fill="currentColor"
-												/>
-											</g>
-											<defs>
-												<clipPath id="clip0_980_24852">
-													<rect width={20} height={20} fill="white" />
-												</clipPath>
-											</defs>
-										</svg>
-									</span>
-									{getDisplayName(category)}
-								</li>
-							))}
-						</ul>
+			{offer ? (
+				<section className="my-8 dark:bg-dark">
+					<div className="mx-4 lg:mx-8 sm:container">
+						<div className="border-r-[5px] border-primary pr-5">
+							<h2 className="mb-4 text-xl lg:text-2xl tracking-tight">
+								يشمل هذا العرض
+							</h2>
+							<ul className="flex flex-col lg:flex-row">
+								{offer?.categories?.map((category) => (
+									<li
+										className="flex text-base text-body-color ml-4"
+										key={category}
+									>
+										<span className="mt-0.5 text-primary">
+											<svg
+												className="ml-2"
+												width={20}
+												height={20}
+												viewBox="0 0 20 20"
+												fill="none"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<g clipPath="url(#clip0_980_24852)">
+													<path
+														d="M10 0.5625C4.78125 0.5625 0.5625 4.78125 0.5625 10C0.5625 15.2188 4.78125 19.4688 10 19.4688C15.2188 19.4688 19.4688 15.2188 19.4688 10C19.4688 4.78125 15.2188 0.5625 10 0.5625ZM10 18.0625C5.5625 18.0625 1.96875 14.4375 1.96875 10C1.96875 5.5625 5.5625 1.96875 10 1.96875C14.4375 1.96875 18.0625 5.59375 18.0625 10.0312C18.0625 14.4375 14.4375 18.0625 10 18.0625Z"
+														fill="currentColor"
+													/>
+													<path
+														d="M12.6875 7.09375L8.96875 10.7188L7.28125 9.0625C7 8.78125 6.5625 8.8125 6.28125 9.0625C6 9.34375 6.03125 9.78125 6.28125 10.0625L8.28125 12C8.46875 12.1875 8.71875 12.2813 8.96875 12.2813C9.21875 12.2813 9.46875 12.1875 9.65625 12L13.6875 8.125C13.9688 7.84375 13.9688 7.40625 13.6875 7.125C13.4063 6.84375 12.9688 6.84375 12.6875 7.09375Z"
+														fill="currentColor"
+													/>
+												</g>
+												<defs>
+													<clipPath id="clip0_980_24852">
+														<rect width={20} height={20} fill="white" />
+													</clipPath>
+												</defs>
+											</svg>
+										</span>
+										{getDisplayName(category)}
+									</li>
+								))}
+							</ul>
+						</div>
 					</div>
-				</div>
-			</section>
+				</section>
+			) : null}
 		</>
 	);
 }
