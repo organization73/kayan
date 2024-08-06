9e6ce6b0c237e1e3d943c8f0506a89409252f6b2
Elsharabasy73 <abdo.make73@gmail.com>
2024-08-06 18:11:35 +0300

    [Add] show the selected category

--- src/components/ShopSection.js
+++ src/components/ShopSection.js
@@ -129,9 +129,9 @@ export default function ShopSection({ offer }) {
 
 				<main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 					<div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-1">
-						<h1 className="text-xl lg:text-2xl tracking-tight text-gray-900">
-							جميع المنتجات
-						</h1>
+					<h1 className="text-xl lg:text-2xl tracking-tight text-gray-900">
+						{selectedCategory}
+				</h1>
 
 						<div className="flex items-center">
 							<Menu as="div" className="relative inline-block text-right">
